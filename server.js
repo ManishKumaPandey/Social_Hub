e31b7ea1011b398755c540976e169a96daa199e9
Manish Kumar Pandey <shmanishmahi@gmail.com>
2026-02-18 23:06:26 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("Social Hub API Running");
+});
+
+
+const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("DB Connection Failed:", error);
+  });
+
+  
+// app.listen(port, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
