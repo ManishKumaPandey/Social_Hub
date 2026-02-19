64ef39f95cc3c41aecd7e142066406b8d929b39c
Manish Kumar Pandey <shmanishmahi@gmail.com>
2026-02-19 17:07:56 +0530

    Save local changes before merge

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("MongoDB Connected");
 
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server ka running on port ${PORT}`);
     });
   })
   .catch((error) => {
