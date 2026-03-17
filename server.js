3c0004cdc6d3057cf480ce3b0853e7f9dca71e1d
Manish Kumar Pandey <shmanishmahi@gmail.com>
2026-03-17 13:15:25 +0530

    restructuring the push

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 app.use(express.json());
 app.use("/api/users", userRouter);
 app.use("/api/posts",postRouter)
